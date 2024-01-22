769993bd6896efa3ffb40ce6c7f8ff0feeb10d83
seemrcola <seemrcola@163.com>
2024-01-22 10:02:10 +0800

    fix: some types

--- src/custom/components/player.tsx
+++ src/custom/components/player.tsx
@@ -1,12 +1,20 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useRef} from "react";
 import videojs from "video.js";
 import "video.js/dist/video-js.css";
 import '@videojs/themes/dist/sea/index.css';
 import db from '../../db'
 
+type Player = ReturnType<typeof videojs>
+
 const Player:React.FC = () => {
+  const player = useRef<Player>()
+  
   function initPlayer(url: string) {
-    videojs('player', {
+    // 销毁实例
+    if (player.current) {
+      player.current.dispose()
+    }
+    player.current = videojs('player', {
       sources:[
         {
           src: url,
@@ -16,8 +24,7 @@ const Player:React.FC = () => {
     });
   }
   
-  useEffect(() => {
-    // 根据recordData字段获取录制的视频
+  function getRecordData() {
     db.table('recordData').toArray().then((recordData: any[]) => {
       // 将所有的base64拿到转成blob
       const allBase64 = recordData.map(item => item.data)
@@ -38,17 +45,20 @@ const Player:React.FC = () => {
       // 初始化播放器
       initPlayer(url)
     })
+  }
+  
+  useEffect(() => {
+    // 根据recordData字段获取录制的视频
+    getRecordData()
   }, [])
   
   return (
-    <div>
+    <div className={'w-1000px h-600px bg-dark'}>
       <video
         id="player"
-        className="video-js vjs-theme-sea"
+        className="video-js vjs-theme-sea w-full h-full"
         controls
         preload="auto"
-        width="1000"
-        height="600"
       />
     </div>
   )
